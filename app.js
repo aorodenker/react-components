1a7c816b4a6834368426a3ccf1923576c5771a79
Andrew <andrew.orodenker@gmail.com>
2022-03-07 12:12:46 -0800

    Complete

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// TODO
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList list={['Potatoes', 'Eggs', 'Bread']}/>
+  </div>
+);
+
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+
+    var style = { //style is a react html
+      fontWeight: this.state.done ? 'bold' : 'normal' //how to bold
+    };
+
+    return (
+      <li style={style} onMouseOver={this.onListItemClick.bind(this)}>{this.props.list}</li>
+    );
+
+  }
+
+}
+
+
+var GroceryList = (props) => (
+  <ul>
+    {props.list.map(list =>
+      <GroceryListItem list={list} />
+    )}
+  </ul>
+);
+
+
+
+
+
+  ReactDOM.render(<App />, document.getElementById("app"));
